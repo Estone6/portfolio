01b1a268e9cd8572542bcce817733fb8897e7a68
Zeeshan Khan <zekhan@dminc.com>
2022-01-16 19:03:13 +0530

    Portfolio SEO changes

---
+++ portfolio.js
@@ -0,0 +1,198 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+  name: "Zeeshan Khan",
+  title: "Hi all, I'm Zeeshan",
+  description:
+    "A passionate Full Stack Web Developer and Mobile App Developer having 3+ yrs experience of building Web applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks and Cross Platform Mobile Apps With React Native.",
+  resumeLink:
+    "https://drive.google.com/file/d/1RcTBSsOk3IMO3Z-cxUSgcNS2f537yNws/view?usp=sharing",
+};
+
+export const openSource = {
+  githubUserName: "Estone6",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+  linkedin: "https://www.linkedin.com/in/its-zeeshan-khan/",
+  github: "https://github.com/Estone6",
+  instagram: "https://www.instagram.com/its_zeeshan_khan/",
+  facebook: "https://www.facebook.com/profile.php?id=100003090777069",
+  twitter: "https://twitter.com/Zeeshan17794626",
+};
+
+export const skillsSection = {
+  title: "What I do",
+  subTitle: "FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive and easy to use Front End / User Interfaces for your web and mobile applications"
+    ),
+    emoji(
+      "⚡ Expertise in React for web and React Native for Mobile Applications"
+    ),
+    emoji(
+      "⚡ Integration of third party services such as Firebase / AWS / Digital Ocean"
+    ),
+  ],
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "vscode-icons:file-type-html",
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "vscode-icons:file-type-css",
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "logos:sass",
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "logos:javascript",
+    },
+    {
+      skillName: "TypeScript",
+      fontAwesomeClassname: "logos:typescript-icon",
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+    },
+    {
+      skillName: "react-native",
+      fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "logos:nodejs-icon",
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "vscode-icons:file-type-npm",
+    },
+    {
+      skillName: "mongoDB",
+      fontAwesomeClassname: "vscode-icons:file-type-mongo",
+    },
+    {
+      skillName: "aws",
+      fontAwesomeClassname: "logos:aws",
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "logos:firebase",
+    },
+    {
+      skillName: "git",
+      fontAwesomeClassname: "logos:git-icon",
+    },
+  ],
+};
+
+export const SkillBars = [
+  {
+    Stack: "Frontend / Design", //Insert stack or technology you have experience in
+    progressPercentage: "90", //Insert relative proficiency in percentage
+  },
+  {
+    Stack: "Backend",
+    progressPercentage: "70",
+  },
+  {
+    Stack: "Programming",
+    progressPercentage: "60",
+  },
+];
+
+export const educationInfo = [
+  {
+    schoolName: "Technocrats Institute of Technology",
+    subHeader: "Bachelor in Information Technology",
+    duration: "August 2015 - May 2019",
+    desc: "Did B.E with stream Information Technology having 7.10 CGPA.",
+    descBullets: [
+      "We made a Shop Management System in the name of major project",
+      "Made an IoT based Garbage Management System in the name of minor project",
+    ],
+  },
+  {
+    schoolName: "RL Public School",
+    subHeader: "Intermediate with Science Stream",
+    duration: "April 2012 - May 2014",
+    desc: "Completed my intermediate with PCM",
+    descBullets: [
+      "Secured First Rank in city level GK competition consecutively",
+      "Did anchoring in multiple school functions",
+    ],
+  },
+];
+
+export const experience = [
+  {
+    role: "Sr. Software Engineer",
+    company: "DMI",
+    companylogo: "/img/icons/common/dmi.png",
+    date: "Oct 2021 – Present",
+    desc: "Working as Software Engineer React having responsibilities of web and mobile projects.",
+  },
+  {
+    role: "Software Engineer",
+    company: "Aerchain",
+    companylogo: "/img/icons/common/aerchain.png",
+    date: "Nov 2019 – Sep 2021",
+    desc: "A procurement solution provider SaaS based company with a team size of 8 tech members.",
+    descBullets: [
+      "Requirement to Deployment ready feature in very efficient manner and minimal time",
+    ],
+  },
+  {
+    role: "Software Engineer Trainee",
+    company: "Mountblue Technologies",
+    companylogo: "/img/icons/common/mountblue.png",
+    date: "Aug 2019 – Oct 2019",
+    desc: "Learned Full Stack Development and multiple techniques and tools to build a production ready app then was deployed to work for a client named Aerchain.",
+  },
+];
+
+export const projects = [
+  {
+    name: "Design Shop",
+    desc: "A full fledged ecommerce app with all the features havin admin and users differentiated view. Starting from product addition, add to cart till payment.",
+    github: "https://github.com/Estone6/DesignShop",
+    link: "http://designshopapp.herokuapp.com/",
+  },
+  {
+    name: "Zomato Clone",
+    desc: "Zomato Clone app in which you search for restaurants, group of restaurants based on location. Filter based on many factors",
+    link: "https://zomato-mb.netlify.app/",
+  },
+  {
+    name: "Chat App",
+    desc: "A Chat App in which you can create room and another person(s) can join the same and chat with each other",
+    link: "https://chat-all.netlify.app/",
+  },
+  {
+    name: "Photo Search",
+    desc: "Photo search wb app using Unsplash API. You can search for photos and see the list",
+    github: "https://github.com/Estone6/PhotoSearchApp",
+    link: "https://photosearchweb.netlify.app/",
+  },
+];
+
+// export const feedbacks = [
+//   {
+//     name: "Hassan Mehmood",
+//     feedback:
+//       "We have been very happy with our new website! It looks professional and very easy to navigate. Our experience with the customer service at Zeeshan Khan has been great. They handle things very efficiently and are available for any questions we have. They also keep us updated on daily reports so we know how our site is doing. I would recommend that u have choose hanzla web developer services for u.",
+//   },
+//   {
+//     name: "Maheen Altaf",
+//     feedback:
+//       "the website is very nice, everything was perfectly made as i need. it just loads in moments, without giving u the chance to wait. the quality is also very amazing. i am using it without any problem. great job",
+//   },
+// ];
